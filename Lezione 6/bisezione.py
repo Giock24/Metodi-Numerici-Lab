7082ff668b53944cdf6f4b67514ae0c7bbccf58e
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2024-08-14 16:26:33 +0200

    Finisched algorithm of Bisezione e Regula Falsi

---
+++ Lezione 6/bisezione.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def bisezione(f ,a, b, tol, nmax):
+    n_it = 0
+    all_xk = []
+    
+    if (f(a)*f(b) < 0):
+        a_0 = a
+        b_0 = b
+        
+        while (np.abs)
+    else:
+        print("Impossibile trovare x t.c f(x)=0")
+    
+    
+    # x = lo zero della funzione
+    # n_it = numero iterazioni fatte
+    # all_xk = tutti gli iterati
+    return x, n_it, all_xk
