049b22896a35e6a94679025b86f01331abf94086
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2024-08-31 12:34:57 +0200

    Finische second simulation + fill some methods until Jacobi

--- UltimaLezioneSimEsame/Scheletriedutilities_20240514/Scheletri_23_24_Versione_Completata.py
+++ UltimaLezioneSimEsame/Scheletriedutilities_20240514/Scheletri_23_24_Versione_Completata.py
@@ -471,164 +471,161 @@ def my_newtonSys_sham(fun, jac, x0, tolx, tolf, nmax):
 
 def my_newton_minimo(gradiente, Hess, x0, tolx, tolf, nmax):
 
-  """
-  DA UTILIZZARE NEL CASO IN CUI CALCOLATE DRIVATE PARZIALI PER GRADIENTE ED HESSIANO SENZA UTILIZZO DI SYMPY
-  
-  Funzione di newton-raphson per calcolare il minimo di una funzione in più variabili
+    """
+    DA UTILIZZARE NEL CASO IN CUI CALCOLATE DRIVATE PARZIALI PER GRADIENTE ED HESSIANO SENZA UTILIZZO DI SYMPY
+    
+    Funzione di newton-raphson per calcolare il minimo di una funzione in più variabili
+    Parametri
+    ----------
+    gradiente : 
+      Nome della funzione che calcola il gradiente della funzione non lineare.
+    Hess :  
+      Nome della funzione che calcola la matrice Hessiana della funzione non lineare.
+    x0 : array
+      Vettore contenente l'approssimazione iniziale della soluzione.
+    tolx : float
+      Parametro di tolleranza per l'errore assoluto.
+    tolf : float
+      Parametro di tolleranza per l'errore relativo.
+    nmax : int
+      Numero massimo di iterazioni.
+    Restituisce
+    -------
+    x : array
+        Vettore soluzione del sistema (o equazione) non lineare.
+    it : int
+        Numero di iterazioni fatte per ottenere l'approssimazione desiderata.
+    Xm : array
+        Vettore contenente la norma del passo ad ogni iterazione.
+    """
 
-  Parametri
-  ----------
-  fun : 
-    Nome della funzione che calcola il gradiente della funzione non lineare.
-  Hess :  
-    Nome della funzione che calcola la matrice Hessiana della funzione non lineare.
-  x0 : array
-    Vettore contenente l'approssimazione iniziale della soluzione.
-  tolx : float
-    Parametro di tolleranza per l'errore assoluto.
-  tolf : float
-    Parametro di tolleranza per l'errore relativo.
-  nmax : int
-    Numero massimo di iterazioni.
-
-  Restituisce
-  -------
-  x : array
-    Vettore soluzione del sistema (o equazione) non lineare.
-  it : int
-    Numero di iterazioni fatte per ottenere l'approssimazione desiderata.
-  Xm : array
-    Vettore contenente la norma del passo ad ogni iterazione.
-  """
-
-  matHess = #to do
-  if #to do:
-    print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
-    return None, None, None
-  grad_fx0= gradiente(x0)    
-  s = #to do
-  # Aggiornamento della soluzione
-  it = 1
-  x1 =#to do
-  grad_fx1 = gradiente(x1)
-  Xm = [np.linalg.norm(s, 1)]
-  
-  while#to do:
-     
-    x0 = #to do
-    it += 1
-    matHess = #to do
-    grad_fx0=grad_fx1
-     
-    if #to do:
-       
-      print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
-      return None, None, None
-      
- 
-    s = #to do
-     
+    matHess = Hess(x0)
+    if np.linalg.det(matHess) == 0:
+        print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
+        return None, None, None
+    grad_fx0= gradiente(x0)    
+    s = np.linalg.solve(matHess, -grad_fx0)
     # Aggiornamento della soluzione
-    x1 = #to do
+    it = 1
+    x1 = x0 + s
+    grad_fx1 = gradiente(x1)
+    Xm = [np.linalg.norm(s, 1)]
+  
+    while it <= nmax and np.linalg.norm(x1 - x0, 1)/np.linalg.norm(x0, 1) > tolx and np.linalg.norm(grad_fx1) > tolf:
+     
+        x0 = x1
+        it += 1
+        matHess = Hess(x0)
+        grad_fx0=grad_fx1
+         
+        if np.linalg.det(matHess) == 0:
+            print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
+            return None, None, None
+          
+     
+        s = np.linalg.solve(matHess, -grad_fx0)
+         
+        # Aggiornamento della soluzione
+        x1 = x0 + s
+    
+        #Calcolo del gradiente nel nuovo iterato
+        grad_fx1  = gradiente(x1)
+        print(np.linalg.norm(s, 1))
+        Xm.append(np.linalg.norm(s, 1))
 
-    #Calcolo del gradiente nel nuovo iterato
-    grad_fx1  = gradiente(x1)
-    print(np.linalg.norm(s, 1))
-    Xm.append(np.linalg.norm(s, 1))
-
-  return x1, it, Xm
+    return x1, it, Xm
 
 def my_newton_minimo_MOD(gradiente, Hess, x0, tolx, tolf, nmax):
 
-  """
-  Funzione di newton-raphson per calcolare il minimo di una funzione in più variabili, modificato nel caso in cui si utilizzando sympy 
-  per calcolare Gradiente ed Hessiano. Rispetto alla precedente versione cambia esclusivamente il modo di valutare il vettore gradiente e la matrice Hessiana in un punto 
-  Parametri
-   ----------
-  fun : 
+    """
+    Funzione di newton-raphson per calcolare il minimo di una funzione in più variabili, 
+    modificato nel caso in cui si utilizzando sympy per calcolare Gradiente ed Hessiano. 
+    Rispetto alla precedente versione cambia esclusivamente il modo di valutare 
+    il vettore gradiente e la matrice Hessiana in un punto 
+    Parametri
+    ----------
+    fun : 
     Nome della funzione che calcola il gradiente della funzione non lineare.
-  Hess :  
+    Hess :  
     Nome della funzione che calcola la matrice Hessiana della funzione non lineare.
-  x0 : array
+    x0 : array
     Vettore contenente l'approssimazione iniziale della soluzione.
-  tolx : float
+    tolx : float
     Parametro di tolleranza per l'errore assoluto.
-  tolf : float
+    tolf : float
     Parametro di tolleranza per l'errore relativo.
-  nmax : int
+    nmax : int
     Numero massimo di iterazioni.
-
-  Restituisce
-  -------
-  x : array
+    Restituisce
+    -------
+    x : array
     Vettore soluzione del sistema (o equazione) non lineare.
-  it : int
+    it : int
     Numero di iterazioni fatte per ottenere l'approssimazione desiderata.
-  Xm : array
+    Xm : array
     Vettore contenente la norma del passo ad ogni iterazione.
-  """
+    """
 
     
-  matHess = np.array([[Hess[0, 0](x0[0], x0[1]), Hess[0, 1](x0[0], x0[1])],
+    matHess = np.array([[Hess[0, 0](x0[0], x0[1]), Hess[0, 1](x0[0], x0[1])],
                       [Hess[1, 0](x0[0], x0[1]), Hess[1, 1](x0[0], x0[1])]])
  
 
-  gradiente_x0=np.array([gradiente[0](x0[0], x0[1]),gradiente[1](x0[0], x0[1])])
+    gradiente_x0=np.array([gradiente[0](x0[0], x0[1]),gradiente[1](x0[0], x0[1])])
    
-  if #to do
-    print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
-    return None, None, None
-      
-  s = #to do
-  
-  # Aggiornamento della soluzione
-  it = 1
-  x1 = #to do
-  grad_fx1=np.array([gradiente[0](x1[0],x1[1]),gradiente[1](x1[0],x1[1])])
-  Xm = [np.linalg.norm(s, 1)]
-  
-  while #to do:
-     
-    x0 = x1
-    it += 1
-    matHess = #to do
-    grad_fx0=grad_fx1
-      
     if np.linalg.det(matHess) == 0:
-       
-      print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
-      return None, None, None
+        print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
+        return None, None, None
       
-
-    
-    s = #to do
-     
+    s = np.linalg.solve(matHess, -gradiente_x0)
+  
     # Aggiornamento della soluzione
-    x1 =#to do
-    #Aggiorno il gradiente per la prossima iterazione 
+    it = 1
+    x1 = x0 + s
     grad_fx1=np.array([gradiente[0](x1[0],x1[1]),gradiente[1](x1[0],x1[1])])
-    print(np.linalg.norm(s, 1))
-    Xm.append(np.linalg.norm(s, 1))
+    Xm = [np.linalg.norm(s, 1)]
+  
+    while it < nmax and np.linalg.norm(x1 - x0, 1)/np.linalg.norm(x1, 1) > tolx and np.linalg.norm(grad_fx0) > tolf:
+     
+        x0 = x1
+        it += 1
+        matHess = np.array([[Hess[0, 0](x0[0], x0[1]), Hess[0, 1](x0[0], x0[1])],
+                      [Hess[1, 0](x0[0], x0[1]), Hess[1, 1](x0[0], x0[1])]])
+        grad_fx0=grad_fx1
+          
+        if np.linalg.det(matHess) == 0:
+            print("La matrice Hessiana calcolata nell'iterato precedente non è a rango massimo")
+            return None, None, None
+          
+        
+        s = np.linalg.solve(matHess, -grad_fx0)
+         
+        # Aggiornamento della soluzione
+        x1 = x0 + s
+        #Aggiorno il gradiente per la prossima iterazione 
+        grad_fx1=np.array([gradiente[0](x1[0],x1[1]),gradiente[1](x1[0],x1[1])])
+        print(np.linalg.norm(s, 1))
+        Xm.append(np.linalg.norm(s, 1))
 
-  return x1, it, Xm
+    return x1, it, Xm
 
 def jacobi(A,b,x0,toll,it_max):
     errore=1000
     d=np.diag(A)
     n=A.shape[0]
     invM=np.diag(1/d)
-    E=#to do
-    F=#to do
-    N=#to do
-    T=#to do
+    E=np.tril(A, -1)
+    F=np.triu(A,+1)
+    N=-(E+F)
+    T=invM@N
     autovalori=np.linalg.eigvals(T)
-    raggiospettrale=#to do
+    raggiospettrale=np.sqrt(np.max(autovalori))
     print("raggio spettrale jacobi", raggiospettrale)
     it=0
     
     er_vet=[]
-    while #to do:
-        x=#to do
+    while it <= it_max and errore > toll:
+        x=T@x0+invM@b
         errore=np.linalg.norm(x-x0)/np.linalg.norm(x)
         er_vet.append(errore)
         x0=x.copy()
